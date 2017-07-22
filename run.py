c672ca6f72123f83c47f797d643b02aa5bf88c87
charlie.you <charlie.you@workday.com>
2017-07-22 14:16:40 -0700

    adsf

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 import argparse
 import time
 
-import Exchange
-import Bot
+from exchange import Exchange
+from bot import Bot
 
 
 def main(strategies, test):
@@ -16,7 +16,7 @@ def main(strategies, test):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('strategies')
-    parser.add_argument('--test', action='store_true')
+    parser.add_argument('--test', action='store_true', default=False)
     args = parser.parse_args()
 
     strategies = args.strategies.split(',')
@@ -25,4 +25,5 @@ if __name__ == '__main__':
             main(strategies, args.test)
         except:
             # TODO metrics for each round
+            print "Sleeping..."
             time.sleep(1)
