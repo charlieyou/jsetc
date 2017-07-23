319aa28ff664d1cf6213bad2198c3c1cf81404ac
charlie.you <charlie.you@workday.com>
2017-07-22 17:16:31 -0700

    mean reversion (actually)

---
+++ strategies/mean_reversion.py
@@ -0,0 +1,45 @@
+from statistics import mean, variance
+
+
+length = 10
+n = 1
+values = {'AAPL': [], 'GOOG': [], 'MSFT': [], 'NOKFH': [], 'NOKUS': [],
+          'XLK': []}
+
+
+def update_values(data, symbol):
+    last_trades = values[symbol]
+    if len(last_trades) > length:
+        last_trades = last_trades[1:]
+    last_trades.append(data['price'])
+
+
+def trade(exchange):
+    trades = []
+    data = exchange.last_data
+    symbol = data['symbol']
+
+    if data['type'] == 'trade':
+        update_values(data, symbol)
+
+    if data['type'] == 'book':
+        if values[symbol]:
+            sma = mean(values[symbol])
+            var = variance(values[symbol], sma)
+            for price, size in data['buy']:
+                # TODO handle case where we hit buy/hold limit
+                # TODO order quantity based on difference (multiple of std?)
+                if price > sma + n * var:
+                    trades.append(('SELL', symbol, price, size))
+                else:
+                    break
+
+            for price, size in data['sell']:
+                # TODO handle case where we hit buy/hold limit
+                # TODO order quantity based on difference (multiple of std?)
+                if price < sma - n * var:
+                    trades.append(('BUY', symbol, price, size))
+                else:
+                    break
+
+    return trades
